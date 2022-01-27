4cbf7404e647dc378fee0c1aaf09232d7ad6008f
Francososa <franky.sosa@gmail.com>
2022-01-27 16:04:27 -0600

    first commit

---
+++ contracts/MijoNFT.sol
@@ -0,0 +1,11 @@
+// SPDX-Licence-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract MijoNFT {
+    constructor() {
+        console.log("Mijo NFT contract. LFG!");
+    }
+}
