fb12e9aaa9bd4188575d34cb1a93e6292f6bcdf3
Francososa <franky.sosa@gmail.com>
2022-01-27 16:21:29 -0600

    Create contract to mint NFTs

--- contracts/MijoNFT.sol
+++ contracts/MijoNFT.sol
@@ -2,10 +2,34 @@
 
 pragma solidity ^0.8.0;
 
+// Import some OpenZeppelin Contracts
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
 
-contract MijoNFT {
-    constructor() {
-        console.log("Mijo NFT contract. LFG!");
-    }
-}
+ // Inherit the imported contract.
+ contract MijoNFT is ERC721URIStorage {
+     // OpenZeppelin magic to keep track of tokenIds.
+     using Counters for Counters.Counter;
+     Counters.Counter private _tokenIds;
+
+     // Need to pass the name of the NFT token and its symbol.
+     constructor() ERC721 ("MijoNFT", "MIJO") {
+         console.log("Mijo NFT contract. LFG!");
+     }
+
+     // A function our user will hit to get their NFT
+     function makeMijoNFT() public {
+         // Get the current tokenId, this starts at 0.
+         uint256 newItemId = _tokenIds.current();
+
+         // Actually mint the NFT to the sender using msg.sender.
+         _safeMint(msg.sender, newItemId);
+
+         // Set the NFTs data.
+         _setTokenURI(newItemId, "blah");
+
+         // Increment the counter for when the next NFT is minted.
+         _tokenIds.increment();
+     }
+ }
